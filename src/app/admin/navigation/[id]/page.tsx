d730f884dd034c46a46183fbe24920130ae9d8b0
gggzyyy[200~# <gggzyyy@gmail.com>
2026-06-13 22:07:33 +0800

    fix: restore useState import corrupted by sed

--- src/app/admin/navigation/[id]/page.tsx
+++ src/app/admin/navigation/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 
+import { useState, useEffect } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import { Button } from "@/registry/new-york/ui/button"
 import { useToast } from "@/registry/new-york/hooks/use-toast"
